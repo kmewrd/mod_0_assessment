69ead326829eb60fc225ec66e40bef601e62358d
Kim Ward <kmewrd@outlook.com>
2021-09-21 22:04:12 -0500

    Add developer model

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var developer = {
+  name: "Kim",
+  favoriteComputer: "Macbook",
+  age: 27,
+  numberOfPets: 1,
+  isIntrovert: true,
+  isThirsty: false,
+  favoriteSnacks: ["almonds", "blueberries", "hummus"],
+  favoriteAnimals: ["otters", "raccoons", "dogs"]
+}
