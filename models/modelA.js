8935a524a778244ad90159db02210dcb47e9997d
Kim Ward <kmewrd@outlook.com>
2021-09-21 22:00:33 -0500

    Add dinosaur model

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var dinosaur = {
+  name: "velociraptor",
+  speed: "25 mph",
+  numberOfFeet: 2,
+  numberOfTeeth: 28,
+  hasTail: true,
+  hasHorn: false,
+  predators: ["alioramus", "troodontids", "oviraptorosaurs"],
+  prey: ["reptiles", "amphibians", "mammals"]
+}
