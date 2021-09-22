b683e5aeca961cd634f6f8cba157473cc5e93fc2
Kim Ward <kmewrd@outlook.com>
2021-09-21 21:48:58 -0500

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,13 @@
+var movie = "The Shining";
+var isEntertaining = true;
+movie = "Carrie";
+var num4 = 4;
+var num6 = 6;
+var product = num6 * num4;
+var favoriteFoods = ["grapes", "cheese", "pasta"];
+
+favoriteFoods.shift();
+movie.length;
+
+// console.log(favoriteFoods);
+// console.log(movie.length);
